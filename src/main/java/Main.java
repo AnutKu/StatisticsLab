434d6b0bea323d79fc2bdb15bff994d01765607b
AnutKu <annyshka.k1608@gmail.com>
2024-04-05 15:36:31 +0300

    Getcolumns

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,16 @@ public class Main {
     public static void main(String args[]){
         MainFrame mainFrame = new MainFrame();
         mainFrame.showFrame();
-        List<Double> listX = ExcelReader.getColumnX();
-        System.out.println(listX);
+        try {
+            Thread.sleep(20000); // Приостановить выполнение на 2000 миллисекунд (2 секунды)
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        List<Double> columnX = ExcelReader.getColumnX();
+        System.out.println(columnX);
+        List<Double> columnY = ExcelReader.getColumnY();
+        System.out.println(columnY);
+        List<Double> columnZ = ExcelReader.getColumnZ();
+        System.out.println(columnZ);
 
 }}
